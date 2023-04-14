a304f933458f9ed53ad12ea44ec41c7747d13714
Lucas Apollo <apollohardrock@gmail.com>
2023-04-14 17:11:07 -0300

    content created

---
+++ blogs/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photoCover": "https://neurosciencenews.com/files/2023/02/brain-atlas-tools-neurosciences-publc.jpg",
+        "title": "Updrage Brain is POSSIBLE!",
+        "description": "New ways to add information to the human brain just by inserting codes per chip have been discovered...",
+    },
+    {
+        "id":"2",
+        "photoCover": "https://bplusmovieblog.files.wordpress.com/2014/10/the-matrix-reloaded-935.png?w=590",
+        "title": "Coming soon election for new leader councilor",
+        "description": "Council leader election: The vote to elect the new council leader took place yesterday. Council members cast their votes and chose a new leader to represent them. The election result will be announced soon. Stay tuned for more information.",
+    },
+    {
+        "id":"3",
+        "photoCover": "https://www.scifibloggers.com/wp-content/uploads/2010/06/zion-Matrix.jpg",
+        "title": "Bill provides for restructuring of Zion",
+        "description": "City restructuring: The city hall announced plans to restructure the city. The project includes the revitalization of public areas and the construction of new infrastructure. The city hall hopes that the changes will improve the quality of life for residents. More details about the project will be released soon.",
+    },
+    {
+        "id":"4",
+        "photoCover": "https://media.sketchfab.com/models/f67eca780c214e08b91038da591bd6a8/thumbnails/da7947a018bb46a8931f93427fed0980/1744527d0e87427daff794d3e064bc1b.jpeg",
+        "title": "Know all the details about the new Nebuchadnezzar 2175",
+        "description": "New spacecraft model launch: The space agency announced the launch of a new spacecraft model. The new spacecraft is equipped with advanced technology and promises to revolutionize space travel. The launch is scheduled for next month and will be broadcast live. Stay tuned for more information.",
+    }
+]
