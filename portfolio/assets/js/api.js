a5cb8b8d97aa53f53896e65ef7131c584097684b
Lucas Apollo <apollohardrock@gmail.com>
2023-05-18 22:03:06 -0300

    change directory

--- portfolio/assets/js/api.js
+++
@@ -1,7 +0,0 @@
-
-
-async function fetchProfileData () {
-    const url = 'https://raw.githubusercontent.com/apollohardrock/projects/main/portfolio/assets/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
